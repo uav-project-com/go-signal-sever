f33e1e559e3989fbd57dbf72fa3c337a4464573d
hieutt <hieutt@hitex.vn>
2025-01-23 14:14:58 +0700

    CORS (not tested yet)

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"boilerplate/api"
 	"boilerplate/api/handler/user"
 	"boilerplate/api/repository"
 	"boilerplate/api/service/user/command"
 	"boilerplate/api/service/user/query"
 	"boilerplate/lib/database"
-	"boilerplate/lib/environment"
-
+	env "boilerplate/lib/environment"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"log"
 )
 
 func main() {
 	// Load environment variables
-	environment.New(0) // Pass 0 if the env file is in the current directory
+	env.New(0) // Pass 0 if the env file is in the current directory
 
 	// Get DSN from environment variables
-	dsn := environment.GetString(environment.DsnKey)
+	dsn := env.GetString(env.DsnKey)
 	if dsn == "" {
 		log.Fatal("DB_DSN not set in environment")
 	}
@@ -44,10 +43,17 @@ func main() {
 
 	// Set up Fiber
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: env.GetString("cors.allow_origins"), // Chỉ cho phép các domain cụ thể
+		AllowHeaders: "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, " +
+			"accept, origin, Cache-Control, X-Requested-With", // Header được phép
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS", // Phương thức HTTP được phép
+		AllowCredentials: true,                              // Cho phép gửi cookies
+	}))
 	api.SetupRoutes(app, userHandler)
 
 	// Get the port from the environment
-	port := environment.GetString(environment.ServicePort)
+	port := env.GetString(env.ServicePort)
 	if port == "" {
 		port = "9090" // Default port if not set
 	}
